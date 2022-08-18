eccbf2e489f61283c6393886b88d0c65ce448f72
rish292 <rishabhchauhan00115@gmail.com>
2022-08-18 15:54:11 +0530

    String variable issue resolved

--- assembler.java
+++ assembler.java
@@ -3,24 +3,35 @@ import java.util.*;
 
 public class Assembler {
 
-    public String s;
+    public String s = "";
 
     public void fileHandler() throws FileNotFoundException, IOException {
         
         Scanner sc = new Scanner(System.in);
         String file = sc.nextLine();    
-        System.out.println("Enter the filename : " + file);   
-        File f = new File(file + ".asm");       //Creates instance of the file taken from input
+        System.out.println("File : " + file + ".asm");   
+        File f = new File(file + ".asm");                               //Creates instance of the file taken from input
         
         BufferedReader br = new BufferedReader(new FileReader(f));
-        s = br.readLine();
+        String str;
             
-        while(s != null) {     //reads bytes from file as long as they're available
+        while((str = br.readLine()) != null) {                         //adds lines read from the file to a string and prints them
                 
-            System.out.println(s);       //prints file contents
-            s = br.readLine();
+            System.out.println(str);       
+            s += (str + "\n");                                         //This string stores all the data of the file
 
         }
+        System.out.println("STRING CHECK : \n" + s);                     /*Output on dry run (also prints the string in whic file data is stored)
+                                                                        code
+                                                                        File : code.asm
+                                                                        ECHO is on.
+                                                                        Sadfh;kvhc
+                                                                        askdfhsa;j
+                                                                        STRING CHECK : 
+                                                                        ECHO is on.
+                                                                        Sadfh;kvhc
+                                                                        askdfhsa;j
+                                                                        */
 
         sc.close();
         br.close();
