0f1f71e6a0d351e1b7235826f28436e54d533355
rish292 <rishabhchauhan00115@gmail.com>
2022-08-18 18:52:18 +0530

    Updated method and var names + Started cleaner

--- assembler.java
+++ assembler.java
@@ -3,27 +3,49 @@ import java.util.*;
 
 public class Assembler {
 
-    public String s = "";
+    public String instructions = "";
+    BufferedReader br;
 
-    public void fileHandler() throws FileNotFoundException, IOException {   //File handler (accesess & reads file)
+    public void file_handler() throws FileNotFoundException, IOException {   //File handler (accesess & reads file)
         
-        Scanner sc = new Scanner(System.in);
-        String file = sc.nextLine();    
-        System.out.println("File : " + file + ".asm");   
-        File f = new File(file + ".asm");                               //Creates instance of the file taken from input
-        
-        BufferedReader br = new BufferedReader(new FileReader(f));
-        String str;
+        while(true) {
             
-        while((str = br.readLine()) != null)                         //adds lines read from the file to a string and prints them    
-            s += (str + "\n");                                       //This string stores all the data of the file
+            Scanner sc = new Scanner(System.in);
+            String in = sc.nextLine() + ".asm";
 
-        System.out.println("File read successfully.");
-        System.out.println("***********************");
+            if(in.exists()) {  
+    
+                System.out.println("File : " + file);   
+                File f = new File(file);                                   //Creates instance of the file taken from input
 
-        sc.close();
-        br.close();
+                br = new BufferedReader(new FileReader(f));
+                String s;
+            
+                while((s = br.readLine()) != null)                         //adds lines read from the file to a string and prints them    
+                    instructions += (s + "\n");                                       //This string stores all the data of the file
+
+                System.out.println("File read successfully.");
+                System.out.println("***********************");
+
+                sc.close();
+                br.close();
+
+            } else {
+                  
+                  System.out.println("ERROR : FILE INVALID");
+                  System.out.println("***********************");
+
+              }
+        }  
+    }
+
+    public void cleaner(){
+		
+        String s = instructions;
+        br = new BufferedReader(new StringReader(s));
+
         
+
     }
 
     public static void main(String args[]) throws FileNotFoundException, IOException {
