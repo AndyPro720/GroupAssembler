a7a8fbdd017f5a3097a038748ae6d81748a77a41
MiteshAgrawal6401 <90169994+MiteshAgrawal6401@users.noreply.github.com>
2022-08-24 15:26:09 +0530

    final edit cleaner

--- assembler.java
+++ assembler.java
@@ -1,10 +1,12 @@
 import java.io.*;
 import java.util.Scanner;
 
-public class assembler {
-   
+public class Assembler {
 
-    public static String filehandler() {
+   public static String instructions;
+
+
+    public static void file_handler() {
         //enter the name of file you want to access
         System.out.println("Enter file name");
         Scanner sc = new Scanner(System.in);
@@ -15,49 +17,45 @@ public class assembler {
         //exception handling
         try {
             Scanner scn = new Scanner(filereader);//var
-            String content="";
             while (scn.hasNextLine()) {
                 String line=scn.nextLine();
-                content = content+line+'\n';
+                instructions = instructions+line+'\n';
             }
             //file is stored in content
-           System.out.println(content);
            sc.close();
         } 
         catch (Exception e) {
             System.out.println("File not found");
             System.out.println(e);
         }
-        return "Done";
+        
     }
 
 
-    public static void Cleaner(){ 
+    public static void cleaner(){ 
 
          //intializing variables
-         String instructions=filehandler();
          String temp ="";
-        
-        //expection handling
-        try {
+         String clean="";
         
             Scanner sci = new Scanner(instructions);
             
             while(sci.hasNextLine()) {
-                temp +=sci.nextLine();                     
+                temp =sci.nextLine();                     
                 if(temp.contains("//")){
-                    temp+= (temp.substring(0, temp.indexOf("//")) + "\n");   
-                }     
+                    clean+= (temp.substring(0, temp.indexOf("//")) + "\n");   
+                }  
+                else{
+                    clean += (temp + "\n");
+                }   
             } 
         
     
-        } catch(Exception e) {
-              System.out.println(e);
-          }
+       
 
           System.out.println("Clearing....");
         
-          instructions =temp.replaceAll("[\t ]*(.?)[\t ]", "");                                  
+          instructions =clean.replaceAll("[\t ]*(.?)[\t ]", "");                                  
           instructions = instructions.strip();                                
       
           System.out.println("After clearing :\n " + instructions);   
@@ -68,9 +66,9 @@ public class assembler {
 
     public static void main(String []args) {
         //filehandler method is used
-        // filehandler();
+         file_handler();
         //cleaning method
-           assembler.Cleaner();
+        cleaner();
         }
       
     
