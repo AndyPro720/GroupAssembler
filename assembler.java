9135f453ad51491b36bd5c8a9fef4d487ffc4aba
MiteshAgrawal6401 <90169994+MiteshAgrawal6401@users.noreply.github.com>
2022-08-25 23:59:37 +0530

    first_pass and symbol table created
    if (label found) : label identifier(key) + instruction_number(value)* is stored in symbol table.part not clearly understood

--- assembler.java
+++ assembler.java
@@ -1,9 +1,42 @@
 import java.io.*;
-import java.util.Scanner;
+import java.util.*;
 
-public class Assembler {
+
+public class Assembler<symbol> {
 
    public static String instructions;
+
+   Map<String,String> symbol = new HashMap<String,String>();
+
+   Assembler() {
+    
+        symbol.put("R0", "0");
+        symbol.put("R1", "1");
+        symbol.put("R2", "2");
+        symbol.put("R3", "3");
+        symbol.put("R4", "4");
+        symbol.put("R5", "5");
+        symbol.put("R6", "6");
+        symbol.put("R7", "7");
+        symbol.put("R8", "8");
+        symbol.put("R9", "9");
+        symbol.put("R10", "10");
+        symbol.put("R11", "11");
+        symbol.put("R12", "12");
+        symbol.put("R13", "13");
+        symbol.put("R14", "14");
+        symbol.put("R15", "15");
+        symbol.put("SCREEN", "16384");
+        symbol.put("KBD", "24576");
+        symbol.put("SP", "0");
+        symbol.put("LCL", "1");
+        symbol.put("ARG", "2");
+        symbol.put("THIS", "3");
+        symbol.put("THAT", "4");
+
+    }
+    
+   
 
 
     public static void file_handler() {
@@ -25,8 +58,7 @@ public class Assembler {
            sc.close();
         } 
         catch (Exception e) {
-            System.out.println("File not found");
-            System.out.println(e);
+            
         }
         
     }
@@ -59,6 +91,30 @@ public class Assembler {
           instructions = instructions.strip();                                
       
           System.out.println("After clearing :\n " + instructions);   
+    }
+
+    public  static void first_pass() {                  
+
+        String temp = "";
+
+        Scanner sci = new Scanner(instructions);
+            String str = "";
+        
+            while(sci.hasNextLine()) {
+                str+=sci.nextLine();
+
+                if(str.startsWith("(")) {           
+                String clean = str.substring(str.indexOf('('), str.indexOf(')'));       
+                continue;         
+                }
+                else{
+                temp += (str + "\n");
+                }
+            }
+
+            instructions= temp.strip();
+           
+
     }
 
 
