94e3b2abb75fc2fa8dafccf632a53ad11a1c197c
Angaj Sharma <trancidence@gmail.com>
2022-09-10 20:52:46 +0530

    Coded test.cpp to transform raw data to map syntax, which was added to Assembler.cpp

--- Assembler.cpp
+++ Assembler.cpp
@@ -9,6 +9,9 @@ class assemble {
    
    std::string instructions; 
    std::unordered_map <std::string, std::string> symbol;
+   std::unordered_map <std::string, std::string> dest;
+   std::unordered_map <std::string, std::string> comp;
+   std::unordered_map <std::string, std::string> jmp;
    
    public:
    assemble();
@@ -113,6 +116,7 @@ class assemble {
          else instructions += line + '\n';
       }
 
+      instructions.erase(instructions.end()-1);  //trims the last newline
       std::cout << "Second Pass Completed, variables stored \n" << "********************** \n";
    }
 
@@ -161,4 +165,58 @@ assemble::assemble() {  //constructor for storing maps
       {"THIS", "3"},
       {"THAT", "4"}, 
    };
+   
+  dest = {
+      {"", "000"},
+      {"M", "001"},
+      {"D", "010"},
+      {"MD", "011"},
+      {"A", "100"},
+      {"AM", "101"},
+      {"AD", "110"},
+      {"AMD", "111"},
+  }; 
+
+   comp = {
+      {"0", "0101010"},
+      {"1", "0111111"},
+      {"-1", "0111010"},
+      {"D", "0001100"},
+      {"A", "0110000"},
+      {"M", "1110000"},
+      {"!D", "0001101"},
+      {"!A", "0110001"},
+      {"!M", "1110001"},
+      {"-D", "0001111"},
+      {"-A", "0110011"},
+      {"-M", "1110011"},
+      {"D+1", "0011111"},
+      {"A+1", "0110111"},
+      {"M+1", "1110111"},
+      {"D-1", "0001110"},
+      {"A-1", "0110010"},
+      {"M-1", "1110010"},
+      {"D+A", "0000010"},
+      {"D+M", "1000010"},
+      {"D-A", "0010011"},
+      {"D-M", "1010011"},
+      {"A-D", "0000111"},
+      {"M-D", "1000111"},
+      {"D&A", "0000000"},
+      {"D&M", "1000000"},
+      {"D|A", "0010101"},
+      {"D|M", "1010101"}, 
+   };
+
+   jmp = {
+      {"", "000"},
+      {"JGT", "001"},
+      {"JEQ", "010"},
+      {"JGE", "011"},
+      {"JLT", "100"},
+      {"JNE", "101"},
+      {"JLE", "110"},
+      {"JMP", "111"},
+   };
+
 }
--- test_code/test.cpp
+++ test_code/test.cpp
@@ -1,14 +1,31 @@
 #include<iostream>
-using namespace std;
+#include<fstream>
+#include<sstream>
+#include<algorithm>
+
 int main() {
-   if (__cplusplus == 201703L)
-      std::cout << "C++17" << endl;
-   else if (__cplusplus == 201402L)
-      std::cout << "C++14" << endl;
-   else if (__cplusplus == 201103L)
-      std::cout << "C++11" << endl;
-   else if (__cplusplus == 199711L)
-      std::cout << "C++98" << endl;
-   else
-      std::cout << "pre-standard C++" << endl;
+   std::fstream i_file_handle;
+   std::string instructions;
+         
+                i_file_handle.open("test.txt", std::ifstream::in); 
+
+                        i_file_handle.seekg(0, std::ios::end); 
+                        instructions.resize(i_file_handle.tellg());
+                        i_file_handle.seekg(std::ios::beg);
+                        i_file_handle.read(&instructions[0], instructions.size()); //read the file and store it
+                     
+                     std::istringstream stream(instructions);
+                     std::string line;
+                     instructions.clear();
+                     while(std::getline(stream, line)) {
+                       std::replace(line.begin(), line.end(), '(', '{'); 
+                       std::replace(line.begin(), line.end(), ')', '}');
+                       std::replace(line.begin(), line.end(), ';', ',');
+                       if(line.size() > 0) instructions += line.substr(line.find('{'), line.find(',')) + '\n';
+                       else instructions += line + '\n'; 
+                     }
+                     std::cout << instructions;
+            
+return 0;
+
 }
